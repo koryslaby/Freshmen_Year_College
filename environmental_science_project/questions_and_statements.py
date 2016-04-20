9d465bdf97e82e28fcac2718faf09180590c0db1
Kory Slaby <kory.slaby@gmail.com>
2016-04-20 02:06:55 -0400

    Environmental_science_project_thirdfile

---
+++ environmental_science_project/questions_and_statements.py
@@ -0,0 +1,90 @@
+# starting question1
+def dirrection1():
+    start1 = input("\nHello there I am here to ask questions and find out how good you are for\n\nthe\
+ Environment. When you are done reading this please hit Enter and I will\n\nbegin\
+ to ask you questions then rate your answer.")
+    print(start1)
+
+statement2 = "\nTo start of, I am giving you the number of points you give yourself because\n\
+altimently it starts and ends with you! Think about this question: Why am \
+I \ngiving myself such a high score? What ever it is take steps to fix it."
+
+statement3 = "\nI said to pick a number between 1 and 10. Since you picked a higher number\
+ you must be realy bad at being invironmentaly friendly. Although you are not\
+nice to mother nature I will be nice and only advance you're score by 10."
+
+statement4 = "\nIts good to think you are Environmentaly friendly but make sure you\n actually are\
+. Think about all the ways that you help the invironment and always be looking for ways \
+to improve."
+
+# starting question2
+def dirrection2():
+    start2 = input("\nOk on to the next question. When you're ready Hit Enter")
+    print(start2)
+
+statement5 = "\nGood going, every time you litter your making the world look worse and\
+ worse!\n\nNot to mention littering is ellegal and Animals can eat or get stuck in the\
+\n\ntrash.\n\nIf litter is small enough to eat, animals will eat it and when they do\
+ there\n\n stomach will be unable to digest the trash and the animal will die of a\n\n full\
+ stomack.\n\nIf the trash is big enough for the animals to get stuck in then they\
+will and\n\n once there stuck they will be easy targets for any preditors near\
+by."
+
+statement6 = "\nI hope you don't litter to much! When you litter that trash \
+makes our world lessbeautiful.\n\nNot to mention littering is ellegal and Animals\
+ can eat or get stuck in\nthe trash.\n\nIf litter is small enough to eat, animals\
+ will eat it and when they do there\n stomach will be unable to digestthe\
+ trash and the animal will die\n of a full stomack.\n\nIf the trash is big\
+enough for the\nanimals to get stuck in then they\
+will and once there stuck they will be easy\n targets for any preditors near by."
+
+
+# question 3
+statement7 = "\nKeep it going!\n\nBy not littering you are doing your part\
+ to keep our world clean.\n\nJust make sure that you are doing the three \
+ R's.\n\nReduce Reuce and Recycle!\n\n"
+
+statement8 = "\nlittering is bad you must stop. By littering you are\
+ poluting our\n\n world and killing innocent creatures\n"
+
+
+# question 4....Facts For thought
+statement9 = "\nWhen someone destroys or disturbs the environment\
+ in any way\n then they could be thinking (well Im only one person, There\
+ are billions.\n I wont make a difference.)\n\n But there are so many people\
+ and when we all think like that it ends up making a huge difference.\n"
+
+
+# question 5
+statement10 = "\nGood riding your bike is a sure way to do your part in cuting\
+ back the burning\n\n of foscil fuels! Co2 emmited from cars are one of the major\n\n\
+ contributors to gobal warming. Review the graph bellow\n\n to see other\
+ ways you can cut down. When you do have to\n\n take the car always make sure\
+ that you are carpooling.\n"
+
+statement11 = "\nCars emmit co2 which is one of the major contributors to global\
+ warming\n\n and the milting of the ice caps. Some places are to fare to walk\
+ but\n\n finding another way to get from B to A is whenever you can is\n\n important\
+ (if you have to take the car try carpooling). If we want\n\n to stop global \
+warming and the changing of are world\n\nThe 10 hotest years\
+ ever recorded have been the last 10!  "
+
+
+# question 6
+statement12 = "\nThis harms your score for the same reason riding in a car does.\
+ ATV's use\n\n gass to run, and the burning of foscil fuels is a big\n\n\
+ contributor of global warming."
+
+
+statement13 = "\nATV's burn foscil fuels and contribute to global warming.\
+Not driving/riding\n\n them helps out your score!"
+
+
+# question7
+statement14 = "\nATV's are big contributors to habitat distruction. Habitats\
+ like Marshes\n\n and rivers are torn up and disturbed by people\n\n on ATV's. Habitat\
+loss is the number one contributor \n\n to Animals and Plants going extinct."
+
+statement15 = "\nHabitat distruction is not only bad for animals. For example, \
+Marsh land\n\nis good for filtering and holding water if \n\nyou alter it enough\
+or distroy it then groung\n\nwater will be more poluted"
