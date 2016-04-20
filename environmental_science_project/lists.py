d38064e8bcb575398665d5be70a6e393878e5712
Kory Slaby <kory.slaby@gmail.com>
2016-04-20 02:12:17 -0400

    Environmental_science_project_tablesfile

---
+++ environmental_science_project/lists.py
@@ -0,0 +1,29 @@
+def improves():
+    improvements = "\nYou Can!\n-------\n- carpool!\n- recycle!\n- reuse!\n- reduce how much you use!\
+\n- walk or ride your bike more!\n- convert to solar or wind power!"
+    print(improvements)
+
+
+def devider():
+    devide = "----------------------------------------------------\n"
+    print(devide)
+
+
+def trash():
+    print("\nWhat Happens to that candy bar wapper!?")
+    print("\nOr that soda can holder?")
+    results = "\nWhere does it go?\n----------------------------------------\
+\n- Eaten by animals\
+(They eventually die)\n\n- Trap Animals (Easy pray for predators)\n\n\
+- If the liter smells good to the Animals the will attempt to eat them\n\
+even if its in the road(hit by cars)\n\n- e.t.c\n"
+    print(results)
+
+
+def travelways():
+    ways = "\nHere are some other ways to geting around!\n\
+----------------------------------------\n- Walking\n\n- Running\n\n\
+- Riding your bike\n\n- Longboarding\n\n- electric car (if you can convert)\
+\n\n "
+    print(ways)
+
