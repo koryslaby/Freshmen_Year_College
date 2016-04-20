59836aa3680d66fa06f87a7ce801a378ec30816b
Kory Slaby <kory.slaby@gmail.com>
2016-04-20 02:03:37 -0400

    Environmental_science_project_mainfile

---
+++ environmental_science_project/project_main.py
@@ -0,0 +1,27 @@
+import project
+import lists
+import questions_and_statements as qs
+
+
+lists.devider()
+qs.dirrection1()
+lists.devider()
+project.question1()
+qs.dirrection2()
+lists.devider()
+project.question2()
+qs.dirrection2()
+lists.devider()
+project.question3()
+qs.dirrection2()
+lists.devider()
+project.fact1()
+qs.dirrection2()
+lists.devider()
+project.question5()
+qs.dirrection2()
+lists.devider()
+project.question6()
+qs.dirrection2()
+lists.devider()
+project.end()
